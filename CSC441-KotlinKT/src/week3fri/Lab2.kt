16d12be6d0fe899f8e04949c4a482a5c9c16b643
Mason McManmon <ryanmason1234@icloud.com>
2026-09-14 17:27:35 -0400

    commented out a repeated section weekday

--- CSC441-KotlinKT/src/week3fri/Lab2.kt
+++ CSC441-KotlinKT/src/week3fri/Lab2.kt
@@ -43,18 +43,18 @@ fun main() {
     println()
 
 
-    val day2 = 3
+//    val day2 = 3
 
-    when (day2) {
-        1 -> println("Monday")
-        2 -> println("Tuesday")
-        3 -> println("Wednesday")
-        4 -> println("Thursday")
-        5 -> println("Friday")
-        6 -> println("Saturday")
-        7 -> println("Sunday")
-        else -> println("Not a day of the week")
-    }
+//    when (day2) {
+//        1 -> println("Monday")
+//        2 -> println("Tuesday")
+//        3 -> println("Wednesday")
+//        4 -> println("Thursday")
+//        5 -> println("Friday")
+//        6 -> println("Saturday")
+//        7 -> println("Sunday")
+//        else -> println("Not a day of the week")
+//    }
 
 
 
